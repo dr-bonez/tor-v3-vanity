febbdff20880bb87bbf012a874510d079383a201
Aiden McClelland <me@drbonez.dev>
2020-07-23 14:27:42 -0600

    todo: accel

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,34 @@ pub fn pubkey_to_onion(pubkey: &[u8; 32]) -> String {
     hasher.update(b".onion checksum");
     hasher.update(pubkey);
     hasher.update(&[3]);
-    let mut onion = Vec::with_capacity(35);
-    onion.extend_from_slice(pubkey);
-    onion.extend_from_slice(&hasher.finalize()[..2]);
-    onion.push(3);
+    let mut onion = [0; 35];
+    onion[..32].clone_from_slice(pubkey);
+    onion[32..34].clone_from_slice(&hasher.finalize()[..2]);
+    onion[34] = 3;
     format!(
         "{}.onion",
-        base32::encode(base32::Alphabet::RFC4648 { padding: false }, &onion).to_lowercase()
+        data_encoding::BASE32_NOPAD.encode(&onion).to_lowercase()
     )
+}
+
+// #[accel::kernel]
+pub fn try_n_from(seed: &mut [u8; 32], uc_prefix: &str, n: usize) -> bool {
+    let mut cur_seed = zkp_u256::U256::from_bytes_be(&seed);
+    let one: zkp_u256::U256 = 1.into();
+    for _ in 0..n {
+        let s = ed25519_compact::Seed::new(cur_seed.to_bytes_be());
+        let kp = ed25519_compact::KeyPair::from_seed(s);
+
+        let mut b32 = [0; 52];
+        data_encoding::BASE32_NOPAD.encode_mut(&*kp.pk, &mut b32);
+        if b32.starts_with(uc_prefix.as_bytes()) {
+            seed.clone_from_slice(&cur_seed.to_bytes_be());
+            return true;
+        }
+
+        cur_seed += one.clone();
+    }
+    false
 }
 
 const FILE_PREFIX: &'static [u8] = b"== ed25519v1-secret: type0 ==\0\0\0";
@@ -51,6 +71,7 @@ fn main() {
             .to_string()
             .into_boxed_str(),
     );
+    let uc_prefix: &'static str = Box::leak(prefix.to_uppercase().into_boxed_str());
     let dst = matches
         .value_of("dst")
         .map(PathBuf::from)
@@ -60,10 +81,9 @@ fn main() {
         prefix.chars().filter(|a| a.is_uppercase()).next().is_none(),
         "prefix must be lowercase"
     );
-    assert!(
-        base32::decode(base32::Alphabet::RFC4648 { padding: false }, prefix).is_some(),
-        "prefix must be base32"
-    );
+    data_encoding::BASE32_NOPAD
+        .decode(prefix.as_bytes())
+        .expect("prefix must be base32");
     let cpus = num_cpus::get();
     let (send, recv) = crossbeam_channel::unbounded();
     let (speed_send, speed_recv) = crossbeam_channel::unbounded();
@@ -71,24 +91,20 @@ fn main() {
         let send = send.clone();
         let speed_send = speed_send.clone();
         std::thread::spawn(move || {
+            use rand::RngCore;
+            let mut seed = [0; 32];
             let mut csprng = rand::thread_rng();
             let mut now = Instant::now();
-            let mut i: usize = 0;
             loop {
-                let kp = ed25519_dalek::Keypair::generate(&mut csprng);
-                let onion = pubkey_to_onion(&kp.public.as_bytes());
-                if onion.starts_with(prefix) {
-                    send.send((onion, kp)).unwrap();
+                csprng.fill_bytes(&mut seed);
+                if try_n_from(&mut seed, uc_prefix, 0x1000) {
+                    send.send(seed).unwrap();
                 }
-                i += 1;
-                if i >= 32768 {
-                    speed_send.send(now.elapsed()).unwrap();
-                    now = Instant::now();
-                    i = 0;
-                }
+                speed_send.send(now.elapsed()).unwrap();
+                now = Instant::now();
             }
         });
-    } //c
+    }
     std::thread::spawn(move || {
         let mut dur = None;
         let mut i = 0;
@@ -96,7 +112,7 @@ fn main() {
         loop {
             let i_dur = speed_recv.recv().unwrap();
             dur = Some(match dur {
-                Some(dur) => (dur * i + i_dur) / (1 + i),
+                Some(dur) => (dur * i + i_dur) / (i + 1),
                 None => i_dur,
             });
             i += 1;
@@ -111,11 +127,13 @@ fn main() {
     loop {
         use std::io::Write;
 
-        let (onion, kp) = recv.recv().unwrap();
+        let seed = recv.recv().unwrap();
+        let kp = ed25519_compact::KeyPair::from_seed(ed25519_compact::Seed::new(seed));
+        let onion = pubkey_to_onion(&*kp.pk);
         println!("{}", onion);
         let mut f = std::fs::File::create(onion).unwrap();
         f.write_all(FILE_PREFIX).unwrap();
-        f.write_all(kp.secret.as_bytes()).unwrap();
+        f.write_all(&*kp.sk).unwrap();
         f.flush().unwrap();
     }
 }
